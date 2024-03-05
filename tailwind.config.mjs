5d83b320c05f17a192be6de6a8ee12cfec4e3c62
Derock <derock@derock.dev>
2024-03-05 17:17:29 -0500

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Atkinson", "sans-serif"],
+    },
+
+    extend: {
+      fontSize: {
+        base: "1.25rem",
+        sm: "1rem",
+        xl: "1.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
