aad9ecd4ef5472b522c585b394170166d7b58157
Derock <derock@derock.dev>
2024-03-06 21:32:52 -0500

    fix: styling & seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://derock.blog",
   integrations: [mdx(), sitemap(), tailwind(), solidJs(), icon()],
 });
