dc94bfbc54076eb5fde2156186790fadfaf42254
Derock <derock@derock.dev>
2024-05-01 16:31:24 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,16 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import remarkMermaid from "astro-diagram/remark-mermaid";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://derock.blog",
   integrations: [mdx(), sitemap(), tailwind(), solidJs(), icon()],
+  markdown: {
+    remarkPlugins: [remarkMermaid, remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }
 });
