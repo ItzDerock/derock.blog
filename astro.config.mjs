50dc211785d9df96d48e774fcd558d3ddf30b21f
Derock <derock@derock.dev>
2024-05-01 20:34:42 -0400

    feat: est read time

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,14 @@ import { defineConfig } from "astro/config";
 import remarkMermaid from "astro-diagram/remark-mermaid";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
+import { remarkReadingTime } from "./src/plugins/readingTime.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://derock.blog",
   integrations: [mdx(), sitemap(), tailwind(), solidJs(), icon()],
   markdown: {
-    remarkPlugins: [remarkMermaid, remarkMath],
+    remarkPlugins: [remarkMermaid, remarkMath, remarkReadingTime],
     rehypePlugins: [rehypeKatex]
   }
 });
